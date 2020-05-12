8aa4dcaa639debf05931de2312ead59eb605e014
farhana akhter <pervez.farhana.outlook.com>
2020-05-11 23:20:20 -0500

    project updated

--- f2a/settings.py
+++ f2a/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django-heroku
 import os
 import environ
 
@@ -120,10 +120,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL='/'
 LOGIN_URL = "login"
+django_heroku.settings(locals())
